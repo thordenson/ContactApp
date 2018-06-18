ce2b5a88038b063067afdf99244238624de30c25
Thor Denson <tjdenson@eckerd.edu>
2018-06-18 11:19:41 -0400

    added different pages

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 const express = require('express');
 const app = express();
 
+
+//Home Page!: Show the user a welcome message
 app.get('/', (req, res) => {
     res.send('HAAAAAAAY');
 });
 
 
+//Contacts List Page: show the user all contacts
+app.get('/contacts', (req, res) => {
+    res.send('You are on the listing page!!');
+});
 
-
+//Contacts Detail Page: show the user all info for one contact
+app.get('/contacts/:id', (req, res)=> {
+    res.send(`You are viewing details for ${req.params.id}`);
+});
 
 
 app.listen(8888, () => {
