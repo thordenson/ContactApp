72c0535239bc33dea78fdaaaf725ebac108a0cc4
Thor Denson <tjdenson@eckerd.edu>
2018-06-18 12:06:35 -0400

    adds handlebars and a couple templates

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const express = require('express');
 const app = express();
 const contacts = require('./contacts');
 
+var expressHbs = require('express-handlebars');
+
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+
+
 
 //Home Page!: Show the user a welcome message
 app.get('/', (req, res) => {
-    res.send('HAAAAAAAY');
+    //res.send('HAAAAAAAY');
+    res.render('home', {
+       message: "Welcome to the contacts app!",
+       headerText: "Contacts App Home Page" 
+
+    });
 });
 
 
@@ -23,7 +36,7 @@ app.get('/contacts/:id', (req, res)=> {
         return user.id == id;
     });
     //TODO: check if `contact` is valid
-    //(meaning is it undefined)
+    //(meaning, is it undefined or a real object)
     res.send(contact);
 });
 
