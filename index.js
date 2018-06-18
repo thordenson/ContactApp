6b41ccdbe5480d7fded445c7a382ac1bc0eb8735
Thor Denson <tjdenson@eckerd.edu>
2018-06-18 11:33:22 -0400

    adds contacts data file and displays info in browser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // installed nodemon, express, express-handlebars
 const express = require('express');
 const app = express();
+const contacts = require('./contacts');
 
 
 //Home Page!: Show the user a welcome message
@@ -11,12 +12,19 @@ app.get('/', (req, res) => {
 
 //Contacts List Page: show the user all contacts
 app.get('/contacts', (req, res) => {
-    res.send('You are on the listing page!!');
+    res.send(contacts.users);
 });
 
 //Contacts Detail Page: show the user all info for one contact
 app.get('/contacts/:id', (req, res)=> {
-    res.send(`You are viewing details for ${req.params.id}`);
+    // res.send(`You are viewing details for ${req.params.id}`);
+    let id = req.params.id;
+    let contact = contacts.users.find((user) => {
+        return user.id == id;
+    });
+    //TODO: check if `contact` is valid
+    //(meaning is it undefined)
+    res.send(contact);
 });
 
 
